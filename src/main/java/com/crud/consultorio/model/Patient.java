e4ff35660da43faf888b942a8baa4fed2506ceda
Edson <edsoneurides0705@gmail.com>
2023-11-13 21:26:30 -0300

    Criação do modelo paciente

---
+++ src/main/java/com/crud/consultorio/model/Patient.java
@@ -0,0 +1,25 @@
+package com.crud.consultorio.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "t_patient")
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String sex;
+    private String cpf;
+    private Date bornDate;
+}
