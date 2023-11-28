73e0bc81406530cd4d7c0cd282db63f8062bdeda
Djonathan <djonathandavila.dev@gmail.com>
2023-11-27 21:08:07 -0300

    Adicionado Payment_type (insert)

---
+++ src/main/java/com/crud/consultorio/model/PaymentType.java
@@ -0,0 +1,21 @@
+package com.crud.consultorio.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "t_payment_type")
+public class PaymentType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String type;
+}
---
+++ src/main/java/com/crud/consultorio/repositories/PaymentTypeRepository.java
@@ -0,0 +1,9 @@
+package com.crud.consultorio.repositories;
+
+import com.crud.consultorio.model.PaymentType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentTypeRepository extends JpaRepository<PaymentType, Integer> {
+}
