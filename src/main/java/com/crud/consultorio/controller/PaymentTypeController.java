8f13424cabcaeb9ff1a0fc1494ea9b163858efa4
Edson <edsoneurides0705@gmail.com>
2023-11-30 19:48:35 -0300

    Finalizado o crud de tipo de pagamento

--- src/main/java/com/crud/consultorio/controller/PaymentTypeController.java
+++ src/main/java/com/crud/consultorio/controller/PaymentTypeController.java
@@ -1,12 +1,11 @@
 package com.crud.consultorio.controller;
 
+import com.crud.consultorio.DTOs.PaymentTypeDTO;
 import com.crud.consultorio.model.PaymentType;
+import com.crud.consultorio.model.Status;
 import com.crud.consultorio.repositories.PaymentTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/paymentType")
@@ -17,5 +16,34 @@ public class PaymentTypeController {
     @PostMapping
     public PaymentType paymentTypeRepository(@RequestBody PaymentType paymentType) {
         return paymentTypeRepository.save(paymentType);
+    }
+
+    @GetMapping
+    public Iterable<PaymentType> gePaymentType(){
+        return paymentTypeRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public PaymentType getPaymentType(@PathVariable int id){
+        return paymentTypeRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/{id}")
+    public PaymentType updatePaymentType(@PathVariable int id, @RequestBody PaymentTypeDTO paymentType){
+        var newPaymentType = paymentTypeRepository.findById(id).orElse(null);
+
+        if (newPaymentType != null){
+
+            newPaymentType.setType(paymentType.getType());
+
+            return paymentTypeRepository.save(newPaymentType);
+        }
+
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePaymentType(@PathVariable int id){
+        paymentTypeRepository.deleteById(id);
     }
 }
